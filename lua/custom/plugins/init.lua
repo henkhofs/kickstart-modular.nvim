8eb2f6776f2771d1a096217a9ef1d0daa6e8e935
henkhofs <henk.hofs@nedap.com>
2024-04-11 16:33:00 +0200

    added my custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,38 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'ThePrimeagen/vim-be-good',
+  },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            -- '.git',
+            --  '.DS_Store',
+            -- 'thumbs.db',
+          },
+          never_show = {},
+        },
+      },
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>el', '<cmd>:Neotree filesystem reveal left<CR>', { desc = 'Neotree reveal filesystem left' })
+      vim.keymap.set('n', '<leader>ec', '<cmd>:Neotree filesystem close <CR>', { desc = 'Neotree close filesystem' })
+    end,
+  },
+}
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -166,10 +166,25 @@ return {
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+
       local servers = {
         -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        gopls = {},
+        java_language_server = {},
+        kotlin_language_server = {},
+        angularls = {},
+        tsserver = {},
+        bashls = {},
+        pyright = {},
+        terraformls = {},
+        cucumber_language_server = {},
+        docker_compose_language_service = {},
+        dockerls = {},
+        gradle_ls = {},
+        graphql = {},
+        html = {},
+        jsonls = {},
+        marksman = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -182,7 +197,7 @@ return {
 
         lua_ls = {
           -- cmd = {...},
-          -- filetypes = { ...},
+          -- filetypes { ...},
           -- capabilities = {},
           settings = {
             Lua = {
@@ -208,7 +223,15 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua', -- Used to format lua code
+        'nginx-language-server',
+        'prettier',
+        'sonarlint-language-server',
+        'trivy',
+        'shellcheck',
+        'shfmt',
+        'tflint',
+        'eslint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
