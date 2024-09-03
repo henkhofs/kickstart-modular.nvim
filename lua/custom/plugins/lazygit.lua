4686ec9f2f9d63e7c4490cf2e444a6163f84e3aa
henkhofs <henk.hofs@nedap.com>
2024-09-03 15:24:27 +0200

    remove esc terminal escape keymap when running lazygit

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -15,6 +15,19 @@ return {
   },
   config = function()
     require('telescope').load_extension 'lazygit'
+    vim.keymap.set('n', '<leader>gg', function()
+      -- if keymap <Esc><Esc> is set in terminal mode, remove it.
+      -- this is to enable <Esc> to navigate in LazyGit which otherwise
+      -- is overridden for terminal usage.
+      local terminal_keymaps = vim.api.nvim_get_keymap 't'
+      -- vim.notify(vim.inspect(terminal_keymaps))
+      for _, keymap in pairs(terminal_keymaps) do
+        if keymap.lhs == '<Esc><Esc>' then
+          vim.api.nvim_del_keymap('t', '<Esc><Esc>')
+        end
+      end
+      vim.cmd 'LazyGit'
+    end, { desc = 'LazyGit', noremap = false, silent = true })
   end,
   -- setting the keybinding for LazyGit with 'keys' is recommended in
   -- order to load the plugin when the command is run for the first time
